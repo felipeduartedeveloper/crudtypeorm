b2969000ec9b896cd3404f67f60a9c375765dd78
Felipe Duarte <felipesilvapac@gmail.com>
2021-09-20 16:04:45 -0300

    chore: UserToken

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -1,9 +1,9 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express';
 import CreateSessionsService from '../services/CreateSessionsService';
 
 export default class SessionsController {
-  public async create (request: Request, response: Response): Promise<Response> {
-    const {email, password} = request.body;
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
 
     const createSession = new CreateSessionsService();
 
