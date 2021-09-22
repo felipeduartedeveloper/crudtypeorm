2bf7ead8e8a706d172430912c17abb9f6c30adcd
Felipe Duarte <felipesilvapac@gmail.com>
2021-09-22 17:48:44 -0300

    chore: Email Send

---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,63 @@
+import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ITemplateVariable {
+  [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
+
+interface ISendMail {
+  to: string;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
+}
+
+export default class EtherealMail {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+
+    const mailTemplate = new HandlebarsMailTemplate();
+
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+
+    const message = await transporter.sendMail({
+      from: {
+        name: from?.name || 'Equipe API',
+        address: from?.email || 'felipesilvapac@gmail.com',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '../../../shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UserRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -23,6 +24,11 @@ class SendForgotPasswordEmailService {
     const token = await userTokensRepository.generate(user.id);
 
     console.log(token);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token.token}`,
+    });
   }
 }
 
