608163c749569495267c2201301694a24f4452b5
Felipe Duarte <felipesilvapac@gmail.com>
2021-10-04 15:56:58 -0300

    chore: migration AddProductIdToOrdersProducts

---
+++ src/shared/typeorm/migrations/1633373776077-AddProductIdToOrdersProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddProductIdToOrdersProducts1633373776077 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
